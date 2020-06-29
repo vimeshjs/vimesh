a72bf45719507a56ef0ffe8fe6f7aa2e330121ce
jacky <xinjie.zhang@gmail.com>
2020-06-29 17:14:42 +0800

    Test Vue.js binding in portlet widget

--- packages/portlet/tests/account/routes/users/me.js
+++ packages/portlet/tests/account/routes/users/me.js
@@ -1,6 +1,8 @@
+const _ = require('lodash')
 function get(req, res) {
     $mock.users.get(req.user.id).then(r => {
-        res.show(r)
+        let options = ['option one', 'option two', 'option three']
+        res.show(_.extend({ options }, r))
     })
 }
 
