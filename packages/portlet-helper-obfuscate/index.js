01e9da202313a2684db6c9ddb78538c58a4859ee
jacky <xinjie.zhang@gmail.com>
2022-01-09 23:18:18 +0800

    Extract javascript obfuscate hbs helper from portlet

---
+++ packages/portlet-helper-obfuscate/index.js
@@ -0,0 +1,49 @@
+const _ = require('lodash')
+const JavaScriptObfuscator = require('javascript-obfuscator')
+
+const obfuscateOptions = {
+    compact: true,
+    controlFlowFlattening: false,
+    controlFlowFlatteningThreshold: 0.75,
+    deadCodeInjection: false,
+    deadCodeInjectionThreshold: 0.4,
+    debugProtection: false,
+    debugProtectionInterval: false,
+    disableConsoleOutput: false,
+    domainLock: [],
+    identifierNamesGenerator: 'mangled',
+    identifiersPrefix: '',
+    inputFileName: '',
+    log: false,
+    renameGlobals: false,
+    reservedNames: [],
+    reservedStrings: [],
+    rotateStringArray: true,
+    seed: 0,
+    selfDefending: false,
+    sourceMap: false,
+    sourceMapBaseUrl: '',
+    sourceMapFileName: '',
+    sourceMapMode: 'separate',
+    stringArray: true,
+    stringArrayEncoding: 'rc4',
+    stringArrayThreshold: 0.75,
+    target: 'browser',
+    transformObjectKeys: false,
+    unicodeEscapeSequence: false
+}
+
+function obfuscate(enabled, options) {
+    if (!options) {
+        options = enabled
+        enabled = true
+    }
+    let content = options.fn(this)
+    return enabled ? '\n' + JavaScriptObfuscator.obfuscate(content, obfuscateOptions) : content
+}
+
+module.exports = (portlet) => {
+    portlet.registerHbsHelpers({
+        obfuscate
+    })
+}
