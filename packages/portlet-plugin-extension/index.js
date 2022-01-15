4e5d93beddd5c53b2a33412b89ada3dcd28a386e
jacky <xinjie.zhang@gmail.com>
2022-01-15 23:33:25 +0800

    Extract extension feature into a plugin

---
+++ packages/portlet-plugin-extension/index.js
@@ -0,0 +1,35 @@
+const _ = require('lodash')
+const { getSortedExtensions } = require('./extensions')
+
+function extensionsByZone(name, options) {
+    let permissions = options.data.root.$permissions || {}
+    let extensionsInZone = options.data.root._extensionsByZone && options.data.root._extensionsByZone[name]
+    let extensions = getSortedExtensions(name, extensionsInZone, permissions)
+    let variable = options.hash.assignTo
+    if (variable) {
+        this[variable] = extensions
+    } else {
+        return JSON.stringify(extensions)
+    }
+}
+
+module.exports = (portlet) => {
+    portlet.ready.extensions = false
+    portlet.allExtensionsByZone = {}
+    portlet.on('decorateResponse', (req, res) => {
+        res.locals._extensionsByZone = portlet.allExtensionsByZone        
+    })
+
+    portlet.on('beforeSetupRoutes', () => {
+        portlet.loadAssets('extensions', '.yaml', (rs) => {
+            _.each(rs, (r, key) => {
+                let pos = key.indexOf('/')
+                let zone = pos == -1 ? key : key.substring(0, pos)
+                portlet.allExtensionsByZone[zone] = _.merge(portlet.allExtensionsByZone[zone], r)
+            })
+            portlet.ready.extensions = true
+        })
+    })
+
+    portlet.registerHbsHelpers({ extensionsByZone })
+}
